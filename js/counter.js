d301099ca2aed1a5e3fb865c3dbb6aad8dc8dd81
smljm < GitHub >
2026-06-24 01:11:47 +0000

    ADi오전 10:112026. 6. 24.1753: {{24AM오전 10:11e}}

--- js/counter.js
+++ js/counter.js
@@ -1,43 +1,42 @@
-/*팝업모달*/
 document.addEventListener("DOMContentLoaded", () => {
   const cards = document.querySelectorAll(".champ-card");
-  const overlay = document.l("modal-wrap");
+  const overlay = document.getElementById("modal-wrap"); // getElementById로 수정
   const modalName = document.getElementById("modal-name");
   const modalType = document.getElementById("modal-type");
   const modalDesc = document.getElementById("modal-desc");
   const closeBtn = document.getElementById("modal-close");
 
-  //클릭시
+  // 클릭 시
   cards.forEach((card) => {
     card.addEventListener("click", () => {
       const name = card.dataset.name;
       const type = card.dataset.type;
       const desc = card.dataset.desc;
 
-      //모달내부텍스트
+      // 모달 내부 텍스트
       modalName.textContent = name;
       modalType.textContent = type === "good" ? "유리한 상대" : "불리한 상대";
 
-      //유리불리색다르게
+      // 유리/불리 색 다르게
       modalType.className = "modal-type " + (type === "good" ? "good" : "bad");
       modalDesc.textContent = desc;
 
-      //모달창열기
+      // 모달 창 열기
       overlay.classList.add("open");
     });
   });
 
-  //모달달기기능
+  // 모달 닫기 기능
   const closeModal = () => overlay.classList.remove("open");
 
   closeBtn.addEventListener("click", closeModal);
 
-  //배경클릭시닫기
+  // 배경 클릭 시 닫기
   overlay.addEventListener("click", (e) => {
     if (e.target === overlay) closeModal();
   });
 
-  //ESC시닫기
+  // ESC 키 누를 시 닫기
   document.addEventListener("keydown", (e) => {
     if (e.key === "Escape") closeModal();
   });
